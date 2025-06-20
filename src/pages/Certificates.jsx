4feb95a989f38ba3b7de10ba6447cc5ad2b8c7b3
sadafshahab12 <sadafshahab123@yahoo.com>
2025-06-19 17:02:02 -0700

    add certificate title

--- src/pages/Certificates.jsx
+++ src/pages/Certificates.jsx
@@ -1,5 +1,8 @@
 import Certificate from "../components/ui/Certificate";
 const Certificates = () => {
+    useEffect(() => {
+    document.title = "Certificates | Sadaf Shahab Portfolio";
+  }, []);
   return (
     <div>
       <Certificate />
